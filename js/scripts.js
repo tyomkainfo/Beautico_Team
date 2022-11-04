4a573a3ca340ac872eb257ece78618ac280133c4
tyomka.info <tyomka.info@gmail.com>
2022-11-04 20:13:37 +0300

    add carusel

---
+++ js/scripts.js
@@ -0,0 +1,34 @@
+$('.owl-carousel_1').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: false,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
+$('.owl-carousel_2').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    navText: [ '', ' ' ],
+    autoplay: false,
+    autoplayTimeout:2000,
+    responsive:{
+        0:{
+            items:2
+        },
+
+        1000:{
+            items:5
+        }
+    }
+});
