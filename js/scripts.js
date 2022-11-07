ed830622e9c4791e7790ea3b4760a5ff493dffe0
W3zker <W3zker@gmail.com>
2022-11-07 22:55:25 +0200

    add header section

--- js/scripts.js
+++ js/scripts.js
@@ -36,3 +36,18 @@ $('.owl-carousel').owlCarousel({
 $( function() {
     $( "#accordion" ).accordion();
 } );
+
+$(document).ready(function () {
+    $('header .hidden_board button').on('click', function () {
+        $('header .hidden_board').hide();
+    })
+
+    $('header .burger').on('click', function () {
+        $('header .burger_menu_wrapper').addClass('active');
+        $('html').css({ overflow: 'hidden' });
+    })
+    $('header .burger_menu_wrapper .close').on('click', function () {
+        $('header .burger_menu_wrapper').removeClass('active');
+        $('html').css({ overflow: 'auto' });
+    })
+})
